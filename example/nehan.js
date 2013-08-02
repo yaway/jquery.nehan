2cbb9814edefae3bdf52a17e9ebbd51757c0c52c
tategakibunko <lambda.watanabe@gmail.com>
2013-08-02 16:05:53 +0900

    update nehan.js

--- example/nehan.js
+++ example/nehan.js
@@ -2432,32 +2432,48 @@ var Tag = (function (){
       return this;
     },
     applyCallbacks : function(context){
+      var css;
       var onload = this.getCssAttr("onload");
       if(onload){
-	onload(this, context);
+	css = onload(this, context);
+	if(css){
+	  this.setCssAttrs(css);
+	}
       }
       var nth_child = this.getCssAttr("nth-child");
       if(nth_child){
-	nth_child(this.getChildNth(), this, context);
+	css = nth_child(this.getChildNth(), this, context);
+	if(css){
+	  this.setCssAttrs(css);
+	}
       }
       var nth_of_type = this.getCssAttr("nth-of-type");
       if(nth_of_type){
-	nth_of_type(this.getChildOfTypeNth(), this, cotext);
+	css = nth_of_type(this.getChildOfTypeNth(), this, cotext);
+	if(css){
+	  this.setCssAttrs(css);
+	}
       }
+/*
       // TODO:
       // nth-last-child, nth-last-of-type not supported yet,
       // because getting order from tail needs 2-pass parsing,
       // and it consts too much for js engine.
-      /*
       var nth_last_child = this.getCssAttr("nth-last-child");
       if(nth_last_child){
-	nth_last_child(this.getLastChildNth());
+	css = nth_last_child(this.getLastChildNth());
+	if(css){
+	  this.setCssAttrs(css);
+	}
       }
       var nth_last_of_type = this.getCssAttr("nth-last-of-type");
       if(nth_last_of_type){
-	nth_last_of_type(this.getLastChildOfTypeNth());
+	css = nth_last_of_type(this.getLastChildOfTypeNth());
+	if(css){
+	  this.setCssAttrs(css);
+	}
       }
-      */
+*/
     },
     setContentRaw : function(content_raw){
       this.contentRaw = content_raw;
